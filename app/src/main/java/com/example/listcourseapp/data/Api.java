c0fbe1bf753c3785f79f51198ee2378b22d304e4
Ashwaganha-cader <max.cacz@yandex.ru>
2022-08-02 11:28:10 +0500

    add api

---
+++ app/src/main/java/com/example/listcourseapp/data/Api.java
@@ -0,0 +1,13 @@
+package com.example.listcourseapp.data;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+
+    @GET("/scripts/XML_dynamic.asp?date_req1=02/07/2022&date_req2=02/08/2022&VAL_NM_RQ=R01235")
+    Call<List<Course>> getMonthCourse();
+
+}
